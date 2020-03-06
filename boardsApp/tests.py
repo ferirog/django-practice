fa1d2b4e88c8d11704bde54e7670b24b7d82337d
ferirog <ferirog@gmail.com>
2020-03-06 22:58:45 +0700

    Fixing some urls and views. Add some new test and topics.html

--- boardsApp/tests.py
+++ boardsApp/tests.py
@@ -1,6 +1,7 @@
 from django.urls import reverse, resolve
 from django.test import TestCase
-from .views import home
+from .views import home, board_topics
+from .models import Board
 
 class HomeTests(TestCase):
     #Testing the status code of the response is 200 (success)
@@ -13,3 +14,28 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+class BoardTopicTest(TestCase):
+    #Prepare the environment to run the test, so to simulate a scenario
+    def setup(self):
+        Board.objects.create(name = 'Django', description = 'Django board')
+    
+    #Testing if Django is returning a status code 200 (success)
+    #for an existing board.
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    #Testing if Django is returning a status code 404 (page not found)
+    #for a board that doesn't exist in the database'
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+    
+    #Testing if Django is using the correct view function 
+    #to render the topics
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEqual(view.func, board_topics)
--- boardsApp/views.py
+++ boardsApp/views.py
@@ -6,3 +6,17 @@ from .models import Board
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+def about(request):
+    # do something...
+    return render(request, 'about.html')
+
+def about_company(request):
+    # do something else...
+    # return some data along with the view...
+    return render(request, 'about_company.html', {'company_name': 'Simple Complex'})
+
+def board_topics(request, pk):
+    # do something...
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board': board})
